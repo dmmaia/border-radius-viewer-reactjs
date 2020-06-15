888388feedb693fd1ca3aec4d5b3865addda0c66
dmmaia <david_mmaia@hotmail.com>
2020-06-15 13:37:32 -0300

    Adding functions about change border-radius, widht and heiht.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [borderRadiusValue, setBorderRadius] = useState();
+  const [widthValue, setWidth] = useState();
+  const [heightValue, setHeight] = useState();
 
   function changeRadius(event){
     event.preventDefault();
@@ -10,13 +12,30 @@ function App() {
     let box = document.getElementById("box-tester");
     box.style.borderRadius = `${borderRadiusValue}px`;
     
-    console.log(borderRadiusValue)
+  }
+
+  function changeWidth(event){
+    event.preventDefault();
+
+    let box = document.getElementById("box-tester");
+    box.style.width = `${widthValue}px`;
+
+  }
+
+  function changeHeight(event){
+    event.preventDefault();
+
+    let box = document.getElementById("box-tester");
+    box.style.height = `${heightValue}px`;
+
   }
 
   return (
     <div className="App">
       <div id = "box-tester" />
+
       <form onSubmit={changeRadius}>
+        <label>Border Radius: </label>
         <input type="number" 
           value={borderRadiusValue}
           onChange={event => setBorderRadius(event.target.value)}
@@ -24,6 +43,27 @@ function App() {
         />
         <input type="submit" value="Apply"/>
       </form>
+
+      <form onSubmit={changeWidth}>
+        <label>Width: </label>
+        <input type="number" 
+          value={widthValue}
+          onChange={event => setWidth(event.target.value)}
+          placeholder="px"
+        />
+        <input type="submit" value="Apply"/>
+      </form>
+
+      <form onSubmit={changeHeight}>
+        <label>Height: </label>
+        <input type="number" 
+          value={heightValue}
+          onChange={event => setHeight(event.target.value)}
+          placeholder="px"
+        />
+        <input type="submit" value="Apply"/>
+      </form>
+
     </div>
   );
 }
